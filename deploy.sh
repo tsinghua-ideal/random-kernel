16642a82e487eb07a6130648768840329258e89e
Chenggang Zhao <lyricz@yeah.net>
2022-11-02 18:12:44 +0800

    Add CNAME configurations

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 git checkout --orphan gh-pages
 npm run build
+echo randomkernel.com > dist/CNAME
 git --work-tree dist add --all
 git --work-tree dist commit -m gh-pages
 git push origin HEAD:gh-pages --force
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/random-kernel/' : '/'
+  // publicPath: process.env.NODE_ENV === 'production' ? '/random-kernel/' : '/'
 })
